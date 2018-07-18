a01a6fd4d6bba8c67f6d479a2941228723f10918
arcadia-ego <arcadia-ego@github.com>
2018-07-18 14:46:40 -0700

    debugging

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const router = require("express").Router();
 const User = require("./userModel.js");
-router.post("/register", (req, res) => {
-  const credentials = req.body;
-  const user = new User(credentials);
-  user
-    .save()
-    .then(inserted => {
-      res.status(201).json(inserted);
-    })
-    .catch(err => res.status(500).json(err));
-});
 
 //var query = { name: 'borne' };
 //Model.findOneAndUpdate(query, { name: 'jason bourne' }, options, callback)
@@ -18,7 +8,8 @@ router.post("/register", (req, res) => {
 
 router
     .route('/update')
-    .get('/update', (req,res) => {
+    .get((req,res) => {
+      console.log("whats up dude");
       res.status(200).json({"whats up": "dude"});
     })
     // .put((req, res) => {
