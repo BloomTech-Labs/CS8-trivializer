98ed9eb0075a4515457b3e83cd47cd9aa539c592
arcadia-ego <arcadia-ego@github.com>
2018-07-25 11:44:22 -0700

    actions localhost fix

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -102,7 +102,7 @@ export const addRound = (round, formProps) => dispatch => {
     dispatch({ type: ADDING_ROUND });
 
     axios
-        .post('http://fathomless-lowlands-45973.herokuapp.com/api/round/create-round', round)
+        .post('http://localhost:5000/api/round/create-round', round)
         .then( response => {
             dispatch({type: ADDED_ROUND, payload: response.data })
         })
