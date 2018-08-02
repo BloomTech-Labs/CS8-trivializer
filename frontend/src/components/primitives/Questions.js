b4ac248c0043ce1527ba84bec7f410612b538028
arcadia-ego <arcadia-ego@github.com>
2018-08-02 11:05:24 -0700

    carlos changes on local pdf

--- frontend/src/components/primitives/Questions.js
+++ frontend/src/components/primitives/Questions.js
@@ -1,43 +1,78 @@
+// import styled from "styled-components";
+
+// export const QuestionsWrapper = styled.div`
+//   /* display: flex; */
+//   flex-direction: column;
+//   justify-content: center;
+//   font-size: 1rem;
+//   /* border: 1px solid black; */
+//   align-items: center;
+//   font-size: 1.6rem;
+//   text-align: center;
+//   /* font-size: 1rem; */
+//   /* color: tomato; */
+  
+// `;
+
+// export const CorrectAnswer = styled.div`
+//   font-weight: 900;
+//   font-size: 1.2rem;
+//   color: purple;
+// `;
+
+// export const QuestionsLine = styled.hr`
+//   border: 1rem solid orange;
+// `;
+// export const QuestionsOuterWrapper = styled.div`
+// color: black;
+// `;
+
+// export const Text = styled.p``;
+
+// export const SubQuestionsWrapper = styled.div`
+//   display: flex;
+//   flex-flow: column wrap;
+//   /* border: 1px solid red; */
+//   margin: 10px;
+//   padding-bottom: 30px;
+//   align-items: center;
+//   /* :hover {
+//     box-shadow: 2px 8px 16px 16px rgba(0, 0, 0, 0.2);
+//   } */
+//   border-bottom: 1px solid black;
+// `;
+
+// // export const TestDiv = styled.div`
+// // display:none;
+// `
+
+
 import styled from "styled-components";
 
+
 export const QuestionsWrapper = styled.div`
-  /* display: flex; */
+  display: flex;
   flex-direction: column;
   justify-content: center;
+  /* align-items: center; */
   font-size: 1rem;
-  /* border: 1px solid black; */
-  align-items: center;
-  font-size: 1.6rem;
-  text-align: center;
+  border: 1px solid black;
+  height: 100vh;
+  /* font-size: 9.9px; */
   /* font-size: 1rem; */
   /* color: tomato; */
-  
 `;
 
 export const CorrectAnswer = styled.div`
   font-weight: 900;
-  font-size: 1.2rem;
+  font-size: 3rem;
   color: purple;
 `;
 
+export const Text = styled.p `
+  font-size: 3rem;
+`
+
 export const QuestionsLine = styled.hr`
-  border: 1rem solid orange;
-`;
-export const QuestionsOuterWrapper = styled.div``;
-
-export const SubQuestionsWrapper = styled.div`
-  display: flex;
-  flex-flow: column wrap;
-  /* border: 1px solid red; */
-  margin: 10px;
-  padding-bottom: 30px;
-  align-items: center;
-  /* :hover {
-    box-shadow: 2px 8px 16px 16px rgba(0, 0, 0, 0.2);
-  } */
-  border-bottom: 1px solid black;
-`;
-
-// export const TestDiv = styled.div`
-// display:none;
-// `
+border: 1rem solid orange;
+` 
